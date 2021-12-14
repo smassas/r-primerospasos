9be4047d7b8d583a7ccb2cfd7d05ab6c961e5fa1
Sebastián Massa Slimming <62157888+smassas@users.noreply.github.com>
2021-12-14 00:36:01 -0300

    Tipo y estructura de datos

--- script.R
+++ script.R
@@ -1,224 +1,73 @@
-# Módulo 1
-# Correo profesor: sebastian.massa@mat.uc.cl
-# Correo ayudante: bahidalgo@uc.cl
-# URL código en vivo: 
-# URL consultas: https://cutt.ly/7Ttmteu
+# Funciones en R ----------------------------------------------------------
 
-# Atajos ------------------------------------------------------------------
+# Diferencia entre log(100, 10) y log(10, 100)
+log(100,10)
+log(10,100)
 
-# Comentarios en R
+# En el primero calculamos el logaritmo de 100 en base 10, en el segundo 
+# calculamos el logaritmo de 10 en base 100
 
-# Cmd + Shift + C (Mac) 
-# Ctrl + Shift + C (Windows).
+# Lo que ya vimos la clase pasada:
 
-# Seccionar líneas de código
+# Objetos en R ----------------------------------------------------------
+# Colección de información indexada.
 
-# Cmd + Shift + R (Mac) 
-# Ctrl + Shift + R (Windows).
+# Creamos un objeto con operador de asignación
+# objeto <- valor
 
-# Instalar y leer librería
+numero <- 256
+resultado <- sqrt(numero)
+resultado
 
-# install.packages("nombre del paquete")
-# library("nombre de librería")
-
-# Ejecutar una línea de código
-
-# Opción 1: Click en botón Run 
-# Opción 2: Ctrl + Enter (Windows)
-# Opción 3: Cmd + Enter (Mac)
-
-# Introducción a R --------------------------------------------------------
-
-# R como calculadora 
-
-5+5
-25/5
-2*2
-27-2
-
-# Operatoria con más cálculos
-
-sqrt(2^4 + exp(3)/55 - log(5*8-2))
-12*(7+2)+(45-32)+8
-30-(-2)*(-10)+(-5)*(-2)
-520 + 202 * log(25)
-2 + 4 * exp(3)
-22^2 - 2^2
-5 + (5 * 10 + 2 * 3)
-log(5) + pi/sqrt(5)
-52 + 203 + 1002 + 204
-
-# R como calculadora lógica
-
-20 == 5 # igualdad
-30 >= 14 # mayor o igual que
-22 <= 2 # menor o igual que
-25 != 10 # no es igual a
-p = 10; y = 5; p <= y # operatoria en objetos
-
-# Actividad 1: Cálculo aritmético -------------------------------------------------------------
-
-520 + 202 * log(25)
-2 + 4 * exp(3)
-2^2* sqrt(2)
-5 + (5 * 10 + 2 * 3)^2
-log(5) + pi/sqrt(5)
-5**2 + 20**3 + 100**2 + 20**4
-
-# Actividad 2: Lógica orientada a objetos -------------------------------------------------------------
-
-# ¿Cuál es el valor de a y b? Si a <- 5; b <- a; a <- 4
-
+# También vimos que podíamos sobreescribir un objeto:
 a <- 5
 b <- a
-a <- 4
-print(a)
+a <- 3
 
-# Sea x = 30, w = 5 y z = a^2, ¿qué resultado obtenemos de x * y + z?
+# ¿Valor de A?
+a
+# ¿Valor de B?
+b
 
-x <- 30
-w <- 5
-z <- a^2
-z*y+z
+# Algunos comando útiles
 
-# Almacenar en un objeto el resultado anterior llamado variable_1.
+ls() # Mostrar objetos creados
+rm(a) # Remueve objeto a
+rm(list = ls()) # Remueve toda la memoria
 
-variable_1 <- z*y+z
+# R es sensible a mayúsculas y minúsculas ---------------------------------
 
-# ¿Qué resultado obtenemos al dividir variable_1 y z?
+polera <- c(254,203,182,50)
+mean(Polera)
+sum(poleras)
 
-variable_1/z
+# Estructuras de datos en R -----------------------------------------------
 
-# Asignar el valor 20000 a la variable presupuesto_2020
+# 1. Vector ---------------------------------------------------------------
+# Se definen con el comando c() y cada elemento se separa con una coma.
+# Son un conjunto de escalares. Un escalar es un valor que posee un 
+# valor fijo como constante. En términos matemáticos, es una matriz con una 
+# dimensión. En términos estadísticos, lo entenderemos como variable.
 
-presupuesto_2020 <- 20000
+vector <- c(60,20,32,45,50)
+class(vector) 
 
-# Ejecutar variable presupuesto_2020
+# Funciones para generar vectores
+rep(1:6, times = 2) # Repetir secuencia 2 veces.
+rep(1:5, each = 2) # Repetir cada número de la secuencia 2 veces.
+rep(1:4, times = c(2,2,2,2)) # Lo mismo que lo anterior
+seq(from = 1, to = 20, by = 2)
 
-presupuesto_2020
+# Funcionalidades adicionales
+length(vector) # Largo de un vector
+is.vector(vector) # Comprueba si la clase se condice con nuestro objeto.
+as.vector(vector) # Transformar un objeto a vector.
+which(seq(from = 1, to = 17, by = 1) > 12)
 
-# Asignar el valor 30000 a la variable presupuesto_2021
+# Seleccionar elemento de un vector
+vector[1] # Extraer el elemento 1 del vector llamado vector
 
-presupuesto_2021 <- 30000
-
-# Ejecutar la variable presupuesto_2021
-
-presupuesto_2021
-
-# Crear nueva variable que sea la sumatoria de presupuesto_2020 y presupuesto_2021
-
-presupuesto_global <- presupuesto_2020 + presupuesto_2021
-
-# Actividad 3: secuencias y repeticiones ----------------------------------
-
-# Construya las siguientes repeticiones usando la función rep, no lo haga ingresando número por número.
-
-# 1 2 3 4 1 2 3 4
-rep(1:4, times = 2)
-rep(seq(from = 1, to = 4, by = 1), 2)
-secuencia <- c(1:4, 1:4)
-
-# 1 1 2 2 3 3 4 4
-rep(1:4, each = 2)
-rep(1:4, times = c(2,2,2,2))
-
-# 1 1 2 3 3 4
-rep(1:4, times = c(2, 1, 2, 1))
-
-# 1 1 2 2 3 3
-rep(1:3, each = 2)
-
-# Una secuencia de dos en dos comenzando en 1 y finalizando en 200.
-seq(from = 1, to = 200, by = 2)
-1:200
-
-# Crear un vector con los números de 1 a 17 y extraer los números que son mayores o iguales a 12.
-1:17 > 12 # Nos entrega el valor lógico.
-which(seq(from = 1, to = 17, by = 1) > 12) # Para entregar la observación y no la posición.
-which(1:17 > 12) 
-
-# Actividad 4: Contagios covid --------------------------------------------
-
-# Número de casos Covid-19 diarios según Comuna.
-
-Las_Condes <- c(80, 90, 50, 40, 35)
-La_Florida <- c(75, 68, 50, 90, 98)
-
-# Crear vector que contenga los días de la semana.
-Dias_contagios <- c("Lunes", "Martes", "Miércoles", "Jueves", "Viernes")
-
-# Asignar los días de la semana al vector Las_Condes y vector La_Florida.
-names(Las_Condes) <- Dias_contagios
-names(La_Florida) <-  Dias_contagios
-
-# Calcule el total de contagios semanales por comuna.
-Total_Las_Condes <- sum(Las_Condes)
-Total_La_Florida <- sum(La_Florida)
-
-# Determinar si los contagios en Las Condes son mayores a La Florida
-
-Total_Las_Condes > Total_La_Florida
-
-# Determinar qué día de la semana se encuentra la mayor cantidad de contagios, 
-# según comuna.
-
-Las_Condes[which.max(Las_Condes)]
-La_Florida[which.max(La_Florida)]
-
-# Elementos adicionales. 
-
-# ¿Qué días fueron mayores a 75 casos diarios por cada comuna?
-
-Las_Condes[which(Las_Condes > 75)]
-La_Florida[which(La_Florida > 75)]
-
-names(Las_Condes)[Las_Condes > 75]
-names(La_Florida)[La_Florida > 75]
-
-# ¿Qué días fueron menores a 40 casos en la comuna de Las Condes?
-Las_Condes[which(Las_Condes < 40)]
-
-# ¿Qué días fueron menores a 68 casos en la comuna de La Florida?
-Las_Condes[which(Las_Condes > 68)]
-
-# Actividad 5: Subsetting -------------------------------------------------
-
-Conteo <- data.frame(Candidato = c("Kast", "Boric"),
-                     Votos = c("64000", "78000"),
-                     Mesas = c("Mesa1", "Mesa1"))
-
-# 1. Seleccionar solo la columna votos
-Conteo[, "Votos"]
-Conteo[, 2]
-
-# 2. Seleccionar la fila 1 y columna 1 y 2.
-Conteo[1, c(1:2)]
-Conteo[1, c(1,2)]
-Conteo[1, c("Candidato", "Votos")]
-
-# 3. Seleccionar columna 2 mediante operador $.
-Conteo$Votos
-
-# 4. Selccionar la fila 1 y columna 1 y 3.
-Conteo[1, c(1,3)]
-Conteo[1, c("Candidato", "Mesas")]
-
-# 5. Seleccionar fila 2, además de la columna 3.
-Conteo[2, 3]
-
-# Síntesis subsetting -----------------------------------------------------
-
-paises <- datos::paises
-
-paises$anio >= 2000 #retornamos TRUE y FALSE
-paises[paises$anio >= 2007,] 
-paises[paises$anio >= 2007, c(1,3)]  #retornemos solo los países y el filtro.
-paises[paises$esperanza_de_vida >= 80, c("pais", "continente")]
-paises[paises$pib_per_capita >= 1000 & paises$continente == "Europa",]
-paises[!(paises$pib_per_capita >= 1000 & paises$continente == "Europa"),]
-subset(x = paises, subset = pais == "Chile", select = c(1:5))
-
-# Actividad 6: Vectores ----------------------------------------------------------------
+# Solución tarea vectores -------------------------------------------------
 
 # 1. Usar "seq()" para crear un vector de 10 números con espacio desde 0 a 12.
 vec_num <- seq(from = 0, to = 12, length.out = 10)
@@ -230,9 +79,11 @@ vec_int <- c(31:40)
 # 3. Usar "LETTERS" ,"[ ]" y "c()" para crear un vector con 9 letras comenzando desde la "C" y que además contenga la "Z"
 vec_cha <- c(LETTERS[3:11], "Z")
 length(vec_cha)
+
 #4. Usar "letters" y "[ ]" para crear un vector de tipo factor con las primeras 10 letras minusculas.
 vec_fac <- factor(letters[1:10])
 length(vec_fac)
+
 # 5. Combinar los vectores obtenidos en (3) y (4) usando "c()". No convertir a factor.
 vec_let <- c(vec_cha, vec_fac)
 
@@ -243,7 +94,21 @@ round(c(vec_num[1:4], vec_int[1:4]), 2)
 # extra: sort()
 sort(round(c(vec_num[1:4], vec_int[1:4]), 2), decreasing = TRUE)
 
-# Actividad 7: Matrices ----------------------------------------------------------------
+# 2. Matrices -------------------------------------------------------------
+
+matrix <- matrix(data = c(20,5,10,15), nrow = 2, ncol = 2)
+colnames(matrix) <- c("Vote", "No vote")
+row.names(matrix) <- c("ABC1", "C2")
+matrix
+
+# Concatenar vectores
+
+v_1 <- 1:5
+v_2 <- LETTERS[1:5]
+cbind(v_1, v_2)
+rbind(v_1, v_2)
+
+# Solución tarea matrices -------------------------------------------------
 
 # 1. Crear una matriz con 10 filas  y 4 columnas llenas de NA usando "matrix()"
 mat.0 <- matrix(nrow = 10, ncol = 4, data = NA)
@@ -257,6 +122,7 @@ mat_2 <- mat_1 # no editar
 dim(mat_1)
 mat_2[,2] <- vec_int
 mat_2
+
 # 4. Asignar "vec_cha" y "vec_fac" a las restantes columnas de "mat_2" para obtener "mat_3".
 mat_3 <- mat_2 # no editar.
 mat_3[,3:4] <- c(vec_cha, vec_fac)
@@ -293,4 +159,80 @@ mat_final
 row.names(mat_final) <- paste("fila", rep(1:20))
 colnames(mat_final) <- paste("columna", rep(1:4))
 mat_final
+
+
+# 3. Dataframe ------------------------------------------------------------
+# Arreglo de datos en una estructura bi-dimensional. A diferencia de una matriz, 
+# un data frame puede tener columnas de distintos tipos de datos. 
+
+# Crear un dataframe
+
+# Forma 1
+mes <- c("Ene", "Feb", "Mar", "Abr", "May")
+anio <- rep(2018, times = length(mes))
+data <- data.frame(anio, mes)
+
+# Forma 2
+data <- data.frame(mes = c("Ene", "Feb", "Mar", "Abr", "May"),
+                   anio = rep(2018, times = length(mes)))
+
+# Actividad práctica
+
+nombre <- c("Andrea", "Bastian", "Camilo", "Daniela")
+grupo_s <- c("AB", "0", "A", "B")
+altura_cm <- c(165, 180, 158, 170)
+
+data <- data.frame(nombre, grupo_s, altura_cm)
+data <- data.frame(nombre = c("Andrea", "Bastian", "Camilo", "Daniela"),
+           grupo_s = c("AB", "0", "A", "B"),
+           altura_cm = c(165, 180, 158, 170))
+
+# Selección de observaciones de un data frame
+
+data[1,] # Fila 1
+data[2, 1] # Fila 2, columna 1.
+data[3, c("altura_cm")] # Fila 3 y columna altura_cm
+data$grupo_s # Rescate con signo peso.
+data[, c("nombre", "altura_cm")]
+
+# Ahora realizarán la siguiente actividad que está en el Drive:
+# Actividad Subsetting -------------------------------------------------
+
+Conteo <- data.frame(Candidato = c("Kast", "Boric"),
+                     Votos = c("64000", "78000"),
+                     Mesas = c("Mesa1", "Mesa1"))
+
+# 1. Seleccionar solo la columna votos
+Conteo[, "Votos"]
+Conteo[, 2]
+
+# 2. Seleccionar la fila 1 y columna 1 y 2.
+Conteo[1, c(1:2)]
+Conteo[1, c(1,2)]
+Conteo[1, c("Candidato", "Votos")]
+
+# 3. Seleccionar columna 2 mediante operador $.
+Conteo$Votos
+
+# 4. Selccionar la fila 1 y columna 1 y 3.
+Conteo[1, c(1,3)]
+Conteo[1, c("Candidato", "Mesas")]
+
+# 5. Seleccionar fila 2, además de la columna 3.
+Conteo[2, 3]
+
+# Operadores lógicos  -----------------------------------------------------
+
+paises <- datos::paises
+
+paises$anio >= 2000 #retornamos TRUE y FALSE
+paises[paises$anio >= 2007,] 
+paises[paises$anio >= 2007, c(1,3)]  #retornemos solo los países, año y el filtro.
+paises[paises$esperanza_de_vida >= 80, c("pais", "continente")]
+paises[paises$pib_per_capita >= 1000 & paises$continente == "Europa",]
+paises[!(paises$pib_per_capita >= 1000 & paises$continente == "Europa"),]
+subset(x = paises, subset = pais == "Chile", select = c(1:5))
+paises[paises$pais %in% c("Chile", "Brasil", "Argentina"),]
+
+
 
